ec16328f68c2ff929ddbc8f6f63f75489f932321
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-21 16:25:36 +0530

    Add files via upload

---
+++ Python_Topic3_Membership_Operators.py
@@ -0,0 +1,22 @@
+# Membership Operators
+# In Place of Question Mark you need to fill with bitwise operator
+
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5 ];
+
+if( ? ):
+   print ("Line 1 - a is available in the given list")
+else:
+   print ("Line 1 - a is not available in the given list")
+
+if ( ? ):
+   print ("Line 2 - b is not available in the given list")
+else:
+   print ("Line 2 - b is available in the given list")
+
+a = 2
+if ( ?  ):
+   print ("Line 3 - a is available in the given list")
+else:
+   print("Line 3 - a is not available in the given list")
